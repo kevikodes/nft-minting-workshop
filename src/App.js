9184c89b9a83cff705a78de5a965ce95e4bc5457
Kevin Owens III <krowens3rd@gmail.com>
2022-02-20 12:46:29 -0800

    ✅ Container Content

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import "./App.css";
 import backgroundVideo from "./assets/background.mp4";
+import nftVideo from "./assets/nftvideo.mp4";
 
 function App() {
   return (
     <div className="wrapper">
+      <video
+        className="video"
+        autoPlay
+        loop
+        playsInline
+        muted
+        src={backgroundVideo}
+        width="600"
+        height="300"
+      />
       <div className="container">
-        <div className="left">Left</div>
+        <div className="left">
+          <video
+            className="nft-video"
+            autoPlay
+            loop
+            playsInline
+            muted
+            src={nftVideo}
+            width="600"
+            height="300"
+          />
+        </div>
         <div className="right">
           <div className="content">
             <h1>Mfer #3424</h1>
             <p>0/100 Minted</p>
-            <button className="mint">Mint</button>
-            <button className="reset">Reset</button>
+            <div className="buttons">
+              <button className="mint">Mint</button>
+              <button className="reset">Start Over</button>
+            </div>
           </div>
         </div>
+        <div className="footer"></div>
       </div>
     </div>
   );
