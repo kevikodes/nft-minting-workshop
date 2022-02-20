19e6828f247fb530bb30ae68659fde924bb523a0
Kevin Owens III <krowens3rd@gmail.com>
2022-02-20 12:14:37 -0800

    ✅ Structure of the containers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <div className="left">Left</div>
+        <div className="right">
+          <div className="content">
+            <h1>Mfer #3424</h1>
+            <p>0/100 Minted</p>
+            <button className="mint">Mint</button>
+            <button className="reset">Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
